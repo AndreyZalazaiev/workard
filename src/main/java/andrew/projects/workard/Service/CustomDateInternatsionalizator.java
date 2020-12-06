7f8a9cc9dc10a5a99852a4854731eb2dbb7c2776
Bergmah <52858177+Bergmah@users.noreply.github.com>
2020-12-06 13:22:32 +0200

    Internationalization dates

---
+++ src/main/java/andrew/projects/workard/Service/CustomDateInternatsionalizator.java
@@ -0,0 +1,35 @@
+package andrew.projects.workard.Service;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+@Component
+public class CustomDateInternatsionalizator extends JsonSerializer<LocalDateTime> {
+
+    @Override
+    public void serialize(LocalDateTime localDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        DateTimeFormatter pattern;
+        String lang = LocaleContextHolder.getLocale().getLanguage();
+
+        if (lang.equalsIgnoreCase("UK")) {
+            pattern = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss", Locale.UK)
+                    .withZone(ZoneId.of("Europe/Kiev"));
+
+        } else {
+            pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
+                    .withZone(ZoneId.of("Europe/London"));
+        }
+        jsonGenerator.writeObject((localDateTime.format(pattern)));
+    }
+}
